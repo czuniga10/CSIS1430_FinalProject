fe40aeabf089c7596d3c797db0f06422941acf59
Chad Zuniga <chad.zuniga10@gmail.com>
2020-07-16 15:03:40 -0600

    added validation to the month

--- htdocs/js-birthday/js/scipt.js
+++ htdocs/js-birthday/js/scipt.js
@@ -1,8 +1,7 @@
 var myFunc = () => {
     var name = prompt("Enter your name:");
     var age = prompt("Enter your age:");
-    var month = prompt("Enter your birth month:").toLowerCase();
-    var season = getSeason(month);
+    var season = getSeason();
 
     alert("Welcome to the Matrix " + name + "! You are " + age + " years old, and you were born in " 
             + season + "!");
@@ -10,18 +9,27 @@ var myFunc = () => {
     document.getElementById("wake-up").innerHTML = "Wake up, " + name + "...";
 }
 
-var getSeason = (month) => {
+var getSeason = () => {
+    var valid = false;
     var season = "";
-    if(month == "march" || month == "april" || month == "may") {
-        season = "the spring";
-    } else if (month == "june" || month == "july" || month == "august") {
-        season = "the summer";
-    } else if (month == "september" || month == "october" || month == "november") {
-        season = "the fall";
-    } else if (month == "december" || month == "january" || month == "february") {
-        season = "the winter";
-    } else {
-        season = "an unknown season";
-    }
+    while(!valid) {
+        var month = prompt("Enter your birth month:").toLowerCase();
+        if(month == "march" || month == "april" || month == "may") {
+            valid = !valid;
+            season = "the spring";
+        } else if (month == "june" || month == "july" || month == "august") {
+            valid = !valid;
+            season = "the summer";
+        } else if (month == "september" || month == "october" || month == "november") {
+            valid = !valid;
+            season = "the fall";
+        } else if (month == "december" || month == "january" || month == "february") {
+            valid = !valid;
+            season = "the winter";
+        } else {
+            alert("Not a valid month. Try again.")
+        }
+    };
+
     return season;
 }
