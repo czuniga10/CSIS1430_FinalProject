a6214c7c15c07bebb95360883e1e98f61fa4dcd7
Chad Zuniga <chad.zuniga10@gmail.com>
2020-08-02 18:34:34 -0600

    game finished with styling

--- htdocs/tic-tac-toe/js/script.js
+++ htdocs/tic-tac-toe/js/script.js
@@ -5,10 +5,16 @@ var winNums = [7,56,73,84,146,273,292,448];
 var markers = ["X","O"];
 var turn = 0;
 var gameOver = false;
+var winCode;
+
+var topChildren;
+var midChildren;
+var botChildren;
 
 function setUpGame() {
     totals = [0,0];
     gameOver = false;
+    winCode = 0;
 
     getPlayer();
     hideButtons();
@@ -16,7 +22,7 @@ function setUpGame() {
     var counter = 1;
     var innerDivs = "";
     for(var i = 1; i <= 3; i++) {
-        innerDivs += `<div class="row-${i}">`; 
+        innerDivs += `<div id="row-${i}">`; 
         for(var j = 1; j <= 3; j++) {
             innerDivs += `<div onclick="playGame(this,${counter});"></div>`
             counter *= 2;
@@ -24,11 +30,33 @@ function setUpGame() {
         innerDivs += `</div>`;
     }
     document.getElementById("game-board").innerHTML = innerDivs;
+
+    topChildren = document.getElementById('row-1').children;
+    midChildren = document.getElementById('row-2').children;
+    botChildren = document.getElementById('row-3').children;
 }
 
 function getNames() {
-    players[0] = prompt("Player 1:");
-    players[1] = prompt("Player 2:");
+    var valid1 = false;
+    var valid2 = false;
+    while(!valid1) {
+        players[0] = prompt("Player 1:");
+        if(!players[0].length) {
+            alert("Name must exist");
+        } else {
+            valid1 = !valid1;
+        }
+    }
+    while(!valid2) {
+        players[1] = prompt("Player 2:");
+        if(!players[1].length) {
+            alert("Name must exist");
+        } else if(players[1].toLowerCase().localeCompare(players[0].toLowerCase()) == 0) {
+            alert(`Name can not be the same as ${players[0]}'s`);
+        } else {
+            valid2 = !valid2;
+        }
+    }
     setUpGame();
 }
 
@@ -47,11 +75,7 @@ function playGame(pickedDiv, val) {
         } else if(gameOver) {
             document.getElementById("title").innerText = "It's a tie";
         } else { 
-            if(turn) {
-                turn = 0;
-            } else {
-                turn = 1;
-            }
+            nextPlayer();
             pickedDiv.attributes[0].nodeValue = "";
             getPlayer();
         }
@@ -63,30 +87,33 @@ function isWin() {
         if((totals[turn] & winNums[i]) == winNums[i]) {
             gameOver = true;
             scoreboard[turn] += 1;
+            winCode = winNums[i];
             showScore();
+            showWinner();
             return true;
         }
     }
     if (totals.reduce((a,b) => a + b) == 511) {
         scoreboard[2] += 1;
+        showTie();
         showScore();
         gameOver = true;
     } else {
         gameOver = false;
     }
-
     return false;
 }
 
 function showScore() {
     showButtons();
-    document.getElementById("scoreboard").innerText = `${players[0]}=${scoreboard[0]}\n${players[1]}=${scoreboard[1]}\nTies=${scoreboard[2]}`
+    document.getElementById("scoreboard").innerHTML = `<div class="sb-player">${players[0]}: ${scoreboard[0]}</div><div class="sb-player">${players[1]}: ${scoreboard[1]}</div><div class="sb-player">Ties: ${scoreboard[2]}</div>`
 }
 
 function showButtons() {
+    document.getElementById("scoreboard-wrapper").style.display = "block";
     document.getElementById("play-again").style.display = "block";
     document.getElementById("switch-users").style.display = "block";
-
+    document.getElementById("game").style.opacity = 4;
 }
 
 function hideButtons() {
@@ -95,10 +122,102 @@ function hideButtons() {
 }
 
 function switchStarter() {
+    nextPlayer();
+    setUpGame();
+}
+
+function nextPlayer() {
     if(turn) {
         turn = 0;
     } else {
         turn = 1;
     }
-    setUpGame();
+}
+
+function showWinner() {
+    switch(winCode) {
+        case 7:
+            topRowWin();
+            break;
+        case 56:
+            midRowWin();
+            break;
+        case 448:
+            botRowWin();
+            break;
+        case 73:
+            leftColWin();
+            break;
+        case 146:
+            midColWin();
+            break;
+        case 292:
+            rightColWin();
+            break;
+        case 273:
+            leftToRightWin();
+            break;
+        case 84:
+            rightToLeftWin();
+            break;
+        default:
+            break;
+    }
+    
+}
+
+function topRowWin() {
+    topChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    topChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    topChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function midRowWin() {
+    midChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function botRowWin() {
+    botChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function leftColWin() {
+    topChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function midColWin() {
+    topChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function rightColWin() {
+    topChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function leftToRightWin() {
+    topChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function rightToLeftWin() {
+    topChildren[2].style.cssText = "background-color : #66FCF1; color : black;";
+    midChildren[1].style.cssText = "background-color : #66FCF1; color : black;";
+    botChildren[0].style.cssText = "background-color : #66FCF1; color : black;";
+}
+
+function showTie() {
+    for(var i = 0; i < 3; i++) {
+        topChildren[i].style.cssText = "background-color : #be0000; color : black;";
+        midChildren[i].style.cssText = "background-color : #be0000; color : black;";
+        botChildren[i].style.cssText = "background-color : #be0000; color : black;";
+    }
 }
